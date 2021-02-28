56f4143ae1bbccc1f745b18df6d75cc00ab10f0a
Fortune 707 <fortune600music@gmail.com>
2021-02-28 14:09:55 +0300

    changed to delphinet

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@ const { mnemonic, secret, password, email } = require("./faucet.json");
 module.exports = {
   networks: {
     development: {
-      host: "https://edonet.smartpy.io",
+      host: "https://delphinet.smartpy.io",
       port: 443,
       network_id: "*",
       secret,
