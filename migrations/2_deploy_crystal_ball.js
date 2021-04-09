35d2457f6e0d36ebf18c82964aa7dc73f2e424e6
Fortune 707 <fortune600music@gmail.com>
2021-04-09 17:32:07 +0300

    added migration settings

--- migrations/2_deploy_crystal_ball.js
+++ migrations/2_deploy_crystal_ball.js
@@ -4,17 +4,33 @@ const { pkh } = require("../faucet.json");
 
 const initialStorage = {
   currencyPair: "XTZ-USD",
-  targetRate: 0,
-  targetTime: "",
-  betsForLedger: new MichelsonMap(),
-  betsAgainstLedger: new MichelsonMap(),
-  oracleAddress: "KT1RCNpUEDjZAYhabjzgz1ZfxQijCDVMEaTZ",
+  createdTime: "1617976565",
+  targetDynamics: 1000000,
+  betsCloseTime: "1618062965", // TODO: get current time here and add 24h
+  measuseStartTime: "",
+  measureOracleStartTime: "",
+  isMeasurementStarted: false,
+  startRate: 0,
+  measurePeriod: 12*60*60,
   isClosed: false,
   closedTime: "",
+  closedOracleTime: "",
   closedRate: 0,
+  closedDynamics: 0,
+  isBetsForWin: false,
+
+  betsForLedger: new MichelsonMap(),
+  betsAgainstLedger: new MichelsonMap(),
+  liquidityLedger: new MichelsonMap(),
+
+  oracleAddress: "KT1SUP27JhX24Kvr11oUdWswk7FnCW78ZyUn",
+
   betsForSum: 0,
   betsAgainstSum: 0,
-  isBetsForWin: false,
+  liquiditySum: 0,
+
+  liquidityPercent: 0,
+  expirationFee: 0,
 };
 
 module.exports = deployer => {
