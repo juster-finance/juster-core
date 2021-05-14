3c23860809cc7e4ce33c81b97994aa3ea9ed4dc0
ztepler <ztepler@gmail.com>
2021-05-14 18:09:26 +0300

    changed script to compile lambda: works

--- compile_and_test.sh
+++ compile_and_test.sh
@@ -1,4 +1,4 @@
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.13.0 compile-contract contracts/main/BakingBet.ligo main > pytezos-tests/baking_bet.tz
-docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.13.0 compile-contract contracts/lambdas/raiseLiquidityFee.ligo lambda > pytezos-tests/lambda_raise_liq_fee.tz
+docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.13.0 compile-expression pascaligo --init-file="contracts/lambdas/raiseLiquidityFee.ligo" lambda > pytezos-tests/lambda_raise_liq_fee.tz
 pytest -v
 
