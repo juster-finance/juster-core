4c24b4f0d84c5def3e35a0429ab8a552fa2fd54a
ztepler <ztepler@gmail.com>
2021-07-21 23:37:21 +0300

    changed event emitter params type to match juster dipdup

--- projects/2021-07-19-juster-maker-proto/event_emitter.py
+++ projects/2021-07-19-juster-maker-proto/event_emitter.py
@@ -6,10 +6,19 @@ import asyncio
 
 class EventCreationEmitter(LoopExecutor):
 
+    DYNAMICS_PRECISION = 1_000_000
+    LIQUIDITY_PRECISION = 1_000_000
+
+
     def __init__(self, period, contract, operations_queue, event_params, next_at):
         """ ...
             - contract: is pytezos object with Juster contract loaded and
                 some key provided
+            - event_params: params of the event
+                - currency_pair: 
+                - target_dynamics: should be float 1.0 - no dynamics
+                - measure_period: in seconds
+                - liquidity_percent: should be float, 0.01 is one percent
         """
 
         # TODO: starts_from = 'datetime unixtime', every=seconds instead of period
@@ -18,9 +27,17 @@ class EventCreationEmitter(LoopExecutor):
 
         self.contract = contract
         self.operations_queue = operations_queue
+        self._verify_event_params(event_params)
         self.event_params = event_params
-        self.currency_pair = event_params['currency_pair']
         self.next_at = next_at
+
+
+    def _verify_event_params(self, event_params):
+        # TODO: make Exceptions
+        assert event_params['target_dynamics'] < 3.0
+        assert event_params['target_dynamics'] > 0.33
+        assert event_params['liquidity_percent'] < 0.3
+        assert event_params['liquidity_percent'] >= 0.0
 
 
     async def create_event(self):
@@ -35,10 +52,10 @@ class EventCreationEmitter(LoopExecutor):
         # creating event:
         event_params = {
             'currencyPair': self.event_params['currency_pair'],
-            'targetDynamics': self.event_params['target_dynamics'],
+            'targetDynamics': int(self.event_params['target_dynamics'] * self.DYNAMICS_PRECISION),
             'betsCloseTime': self.next_at + self.event_params['bets_period'],
             'measurePeriod': self.event_params['measure_period'],
-            'liquidityPercent': self.event_params['liquidity_percent'],
+            'liquidityPercent': int(self.event_params['liquidity_percent'] * self.LIQUIDITY_PRECISION),
         }
 
         # TODO: should I move this fees from event_params into event emitter params?
