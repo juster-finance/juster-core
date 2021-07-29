cb1d92c80677312b4c9f7a7baf7e4570b3332224
ztepler <ztepler@gmail.com>
2021-07-29 20:39:27 +0300

    fixed this test because catched another failwith's

--- tests/withdraw_unknown_test.py
+++ tests/withdraw_unknown_test.py
@@ -13,29 +13,7 @@ class WithdrawUnknownTest(JusterBaseTestCase):
 
         amount = self.measure_start_fee + self.expiration_fee
 
-        # Creating empty event with no participants:
-        self.new_event(
-            event_params=self.default_event_params, amount=amount)
-
-        # Forced finish:
-        self.storage['events'][0].update({
-            'isClosed': True,
-            'isBetsAboveEqWin': True
-        })
-
-        # Withdraw is not succeed:
-        with self.assertRaises(MichelsonRuntimeError) as cm:
-            self.withdraw(self.a, 1_000_000)
-        msg = 'Participant not found'
-        self.assertTrue(msg in str(cm.exception))
-
-
-    def test_withdraw_twice(self):
-        """ Test that witdrawing second time would raise error """
-
-        amount = self.measure_start_fee + self.expiration_fee
-
-        # Making event with provided liquidity:
+        # Creating empty event with one paritcipant:
         self.new_event(
             event_params=self.default_event_params, amount=amount)
 
@@ -52,12 +30,50 @@ class WithdrawUnknownTest(JusterBaseTestCase):
             'isBetsAboveEqWin': True
         })
 
+        # B is not participated so withdraw should fail:
+        with self.assertRaises(MichelsonRuntimeError) as cm:
+            self.withdraw(self.b, 0)
+        msg = 'Participant not found'
+        self.assertTrue(msg in str(cm.exception))
+
+
+    def test_withdraw_twice(self):
+        """ Test that witdrawing second time would raise error """
+
+        amount = self.measure_start_fee + self.expiration_fee
+
+        # Making simple event with two providers (need two so event would
+        # not be removed after first withdrawal):
+        self.new_event(
+            event_params=self.default_event_params, amount=amount)
+
+        # A provides liquidity with 1 tez:
+        self.provide_liquidity(
+            participant=self.a,
+            amount=1_000_000,
+            expected_above_eq=1,
+            expected_below=1)
+
+        # B provides liquidity with 1 tez:
+        self.provide_liquidity(
+            participant=self.b,
+            amount=1_000_000,
+            expected_above_eq=1,
+            expected_below=1)
+
+        # Forced finish:
+        self.storage['events'][0].update({
+            'isClosed': True,
+            'isBetsAboveEqWin': True
+        })
+
         # First withdraw is succeed:
         self.withdraw(self.a, 1_000_000)
 
         # Second raises error:
         with self.assertRaises(MichelsonRuntimeError) as cm:
             self.withdraw(self.a, 1_000_000)
+
         msg = 'Participant not found'
         self.assertTrue(msg in str(cm.exception))
 
