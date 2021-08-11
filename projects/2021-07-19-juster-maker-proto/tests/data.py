feb9f8c1d3eed49f1d8ed13d2653bc72483aec12
ztepler <ztepler@gmail.com>
2021-08-11 10:57:18 +0300

    added another dipdup client test that processed default event response

--- projects/2021-07-19-juster-maker-proto/tests/data.py
+++ projects/2021-07-19-juster-maker-proto/tests/data.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 
 # TODO: should it be easier to have separate object with default constructor?
-DEFAULT_EVENT_PARAMS = params = {
+DEFAULT_EVENT_PARAMS = {
     'currency_pair': 'XTZ-USD',
     'target_dynamics': 1.0,
     'bets_period': 900,
@@ -11,5 +11,15 @@ DEFAULT_EVENT_PARAMS = params = {
     'expiration_fee': 100_000,
     'measure_start_fee': 100_000,
     'bets_close_time': datetime.now()
+}
+
+
+DEFAULT_DIPDUP_EVENT_RESPONSE = {
+    'data': {
+        'juster_event': [{
+            'created_time': '2021-08-09T11:00:50.357847+00:00',
+            'bets_close_time': '2021-06-22T11:55:00+00:00'
+        }]
+    }
 }
 
--- projects/2021-07-19-juster-maker-proto/tests/dipdup_client_test.py
+++ projects/2021-07-19-juster-maker-proto/tests/dipdup_client_test.py
@@ -2,9 +2,11 @@ import asyncio
 from unittest import TestCase
 
 from dipdup import JusterDipDupClient
-from unittest.mock import MagicMock
+from unittest.mock import Mock
 import tests.config as config
 from urllib.error import URLError
+from http.client import RemoteDisconnected
+from tests.data import DEFAULT_DIPDUP_EVENT_RESPONSE
 
 
 class DipDupClientTest(TestCase):
@@ -12,8 +14,8 @@ class DipDupClientTest(TestCase):
         self.loop = asyncio.get_event_loop()
 
 
-    def test_should_make_multiple_attempts_with_URLError(self):
-        mock_endpoint = MagicMock(side_effect=URLError('TODO: add reason here'))
+    def test_should_make_multiple_attempts_with_URLError_and_then_fail(self):
+        mock_endpoint = Mock(side_effect=URLError('TODO: add reason here'))
         dd_client = JusterDipDupClient(config)
         dd_client.endpoint = mock_endpoint
 
@@ -21,4 +23,21 @@ class DipDupClientTest(TestCase):
             self.loop.run_until_complete(dd_client.make_query('all_events'))
 
         self.assertEqual(mock_endpoint.call_count, config.MAX_RETRY_ATTEMPTS)
+
+
+    def test_should_retry_if_RemoteDisconnected_and_succeed(self):
+
+        raise_three_errors_and_then_succeed = [
+            RemoteDisconnected('TODO: add reason here'),
+            RemoteDisconnected('TODO: add reason here'),
+            RemoteDisconnected('TODO: add reason here'),
+            DEFAULT_DIPDUP_EVENT_RESPONSE
+        ]
+
+        mock_endpoint = Mock(side_effect=raise_three_errors_and_then_succeed)
+        dd_client = JusterDipDupClient(config)
+        dd_client.endpoint = mock_endpoint
+
+        self.loop.run_until_complete(dd_client.make_query('all_events'))
+        self.assertEqual(mock_endpoint.call_count, 4)
 
