b412553ac6921c9975fed5fce285c969575bcb15
ztepler <ztepler@gmail.com>
2021-10-22 18:15:45 +0300

    looks like model working but have exploit as expected

--- projects/2021-10-22-juster-b/test_juster_b.py
+++ projects/2021-10-22-juster-b/test_juster_b.py
@@ -32,3 +32,36 @@ def test_some_liquidity_removed_and_then_some_bet_placed():
     jb.give_reward(insurance_two)
     jb.assert_empty()
 
+
+def test_where_pools_turn_over():
+    jb = JusterB.new_with_deposit('A', 1, 2)
+    insurance_one = jb.insure('B', 1, 'for')
+    jb.provide_liquidity('A', 2)
+    jb.give_reward(insurance_one)
+    insurance_two = jb.insure('C', 4, 'against')
+    jb.remove_liquidity('A', 4)
+    jb.give_reward(insurance_two)
+    jb.assert_empty()
+
+
+def test_with_succeeded_insurance_claim():
+    jb = JusterB.new_with_deposit('A', 1000, 1000)
+    insurance_one = jb.insure('B', 10, 'for')
+    jb.claim_insurance_case()
+    jb.remove_liquidity('A', 1000)
+    jb.give_reward(insurance_one)
+    jb.assert_empty()
+
+
+def test_where_provider_exploits_insurance_case():
+    jb = JusterB.new_with_deposit('A', 1000, 1000)
+    insurance_one = jb.insure('B', 10, 'for')
+    jb.remove_liquidity('A', 1000)
+    jb.claim_insurance_case()
+    jb.give_reward(insurance_one)
+
+    # because both A and B "wins" - there are divergent balance:
+    assert sum(jb.balances.values()) > 10
+
+    # This is why it is required to implement SHARE LOCK (or another mechanics)
+
