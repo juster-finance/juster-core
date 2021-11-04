754a4d5ec55b1c2c012bfe9101db49d8a620fe64
ztepler <ztepler@gmail.com>
2021-11-04 20:48:31 +0300

    researching properties with different ratios and they are very strange

--- projects/2021-11-03-juster-c/test_juster_c.py
+++ projects/2021-11-03-juster-c/test_juster_c.py
@@ -12,6 +12,7 @@
 # pool ratio used to calculate insurance deltas, pool works the same way it works in Juster
 
 import pytest
+from pools import Pools
 from juster_c import JusterC
 
 
@@ -98,9 +99,80 @@ def test_when_provider_get_into_and_get_out_during_event():
     jc.assert_empty()
 
 
-@pytest.mark.skip(reason='under development')
-def test_where_providers_have_very_different_ratios():
-    pass
+def test_where_providers_have_very_different_ratios_win_case():
+    jc = JusterC(duration=100)
+
+    # Three providers are participated in the event:
+    jc.provide('A', amount_for=4,  amount_against=48)  # ratio  0.083
+    jc.provide('B', amount_for=48, amount_against=48)  # ratio  1.000
+    jc.provide('C', amount_for=48, amount_against=4)   # ratio 12.000
+    assert jc.pools == Pools(100, 100)
+
+    # D insures for really big amount and moved pools 100:50
+    insurance = jc.insure('D', 100)
+    assert jc.pools == Pools(100, 50)
+
+    jc.wait(100)
+    jc.dissolve(insurance)
+
+    # Providers win case:
+    lock_a = jc.lock('A',  4, 48)
+    lock_b = jc.lock('B', 48, 48)
+    lock_c = jc.lock('C', 48,  4)
+    jc.withdraw(lock_a)
+    jc.withdraw(lock_b)
+    jc.withdraw(lock_c)
+
+    # Providers splitting 100 from D by AGAINST pool
+    # profit distributed proportional to the provided liquidity, ratio does not
+    # matter (is it good property or not?):
+    jc.assert_balances_equal({
+        'A': (48 +  4) / 200 * 100,
+        'B': (48 + 48) / 200 * 100,
+        'C': ( 4 + 48) / 200 * 100
+    })
+
+    jc.assert_empty()
+
+
+def test_where_providers_have_very_different_ratios_lose_case():
+    jc = JusterC(duration=100)
+
+    # Three providers are participated in the event:
+    jc.provide('A', amount_for=4,  amount_against=48)  # ratio  0.083
+    jc.provide('B', amount_for=48, amount_against=48)  # ratio  1.000
+    jc.provide('C', amount_for=48, amount_against=4)   # ratio 12.000
+    assert jc.pools == Pools(100, 100)
+
+    # D insures for really big amount and moved pools 100:50
+    insurance = jc.insure('D', 100)
+    assert jc.pools == Pools(100, 50)
+
+    jc.wait(12)
+    jc.claim_insurance_case()
+    jc.reward(insurance)
+
+    # Providers lose case:
+    lock_a = jc.lock('A',  4, 48)
+    lock_b = jc.lock('B', 48, 48)
+    lock_c = jc.lock('C', 48,  4)
+    jc.withdraw(lock_a)
+    jc.withdraw(lock_b)
+    jc.withdraw(lock_c)
+
+    # Wow wow wow, looks like C in profit here? wtf
+    # so then, does it mean that provider should just add all liquidity to FOR pool?
+    # ^^ need to test that clearly exploit this
+
+    # Simplified profit formula: splitted FOR pool - provided AGAINST
+    jc.assert_balances_equal({
+        'A': 0.04 * 50 - 48,
+        'B': 0.48 * 50 - 48,
+        'C': 0.48 * 50 - 4
+    })
+
+    jc.assert_empty()
+
 
 # TODO: test where providers have very different ratios, one with 10:1, one 1:1 and one 1:10
 # TODO: test where providers have lock with different ratios
