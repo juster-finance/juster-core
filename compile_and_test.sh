bbb8e5fcd03c25e55d286ee44032eefa8605b076
ztepler <ztepler@gmail.com>
2021-11-08 14:54:23 +0300

    cleaning up, moved WIP contracts code into experimental directory + ignoring projects dir when running tests

--- compile_and_test.sh
+++ compile_and_test.sh
@@ -1,10 +1,10 @@
-docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-contract contracts/main/Juster-B.ligo main > build/tz/juster_b.tz
+# docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-contract contracts/experimental/Token.ligo main > build/tz/token.tz
+# docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-contract contracts/experimental/Juster-B.ligo main > build/tz/juster_b.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-contract contracts/main/OracleMock.ligo main > build/tz/oracle_mock.tz
-docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-contract contracts/main/Token.ligo main > build/tz/token.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-contract contracts/main/Juster.ligo main > build/tz/juster.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-expression pascaligo --init-file="contracts/lambdas/raiseLiquidityFee.ligo" lambda > build/tz/lambda_raise_liq_fee.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-expression pascaligo --init-file="contracts/lambdas/resetConfig.ligo" lambda > build/tz/lambda_reset_new_event_config.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-expression pascaligo --init-file="contracts/lambdas/changeOracle.ligo" lambda > build/tz/lambda_change_oracle.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-expression pascaligo --init-file="contracts/lambdas/triggerPauseEvents.ligo" lambda > build/tz/lambda_trigger_pause.tz
-pytest -v
+pytest -v --ignore=projects
 
