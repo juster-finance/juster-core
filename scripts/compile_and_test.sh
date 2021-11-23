3aa9b875f7766cdba9ad20c67b35c01924ca7bb6
ztepler <ztepler@gmail.com>
2021-11-23 16:26:09 +0300

    removed experimental + updated ligo compiler to 0.29

--- scripts/compile_and_test.sh
+++ scripts/compile_and_test.sh
@@ -1,10 +1,8 @@
-# docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-contract contracts/experimental/Token.ligo main > build/tz/token.tz
-# docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-contract contracts/experimental/Juster-B.ligo main > build/tz/juster_b.tz
-docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-contract contracts/main/OracleMock.ligo main > build/tz/oracle_mock.tz
-docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-contract contracts/main/Juster.ligo main > build/tz/juster.tz
-docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-expression pascaligo --init-file="contracts/lambdas/raiseLiquidityFee.ligo" lambda > build/tz/lambda_raise_liq_fee.tz
-docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-expression pascaligo --init-file="contracts/lambdas/resetConfig.ligo" lambda > build/tz/lambda_reset_new_event_config.tz
-docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-expression pascaligo --init-file="contracts/lambdas/changeOracle.ligo" lambda > build/tz/lambda_change_oracle.tz
-docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.23.0 compile-expression pascaligo --init-file="contracts/lambdas/triggerPauseEvents.ligo" lambda > build/tz/lambda_trigger_pause.tz
+docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.29.0 compile contract contracts/main/OracleMock.ligo -e main > build/tz/oracle_mock.tz
+docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.29.0 compile contract contracts/main/Juster.ligo -e main > build/tz/juster.tz
+docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.29.0 compile expression pascaligo lambda --init-file "contracts/lambdas/raiseLiquidityFee.ligo" > build/tz/lambda_raise_liq_fee.tz
+docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.29.0 compile expression pascaligo lambda --init-file "contracts/lambdas/resetConfig.ligo" > build/tz/lambda_reset_new_event_config.tz
+docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.29.0 compile expression pascaligo lambda --init-file "contracts/lambdas/changeOracle.ligo" > build/tz/lambda_change_oracle.tz
+docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.29.0 compile expression pascaligo lambda --init-file "contracts/lambdas/triggerPauseEvents.ligo" > build/tz/lambda_trigger_pause.tz
 pytest -v --ignore=projects
 
