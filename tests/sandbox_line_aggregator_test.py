45c331e6dedc324baba010819b23cc0bf982dd7c
ztepler <ztepler@gmail.com>
2021-11-26 09:31:16 +0300

    test that confirm possibility of event creation with provided liquidity in the one batch

--- tests/sandbox_line_aggregator_test.py
+++ tests/sandbox_line_aggregator_test.py
@@ -38,8 +38,6 @@ class SandboxLineAggregatorTestCase(SandboxedJusterTestCase):
     def _aggregator_create_event(self, user, lineId=0):
         opg = (user.contract(self.line_aggregator.address)
             .createEvent(lineId)
-            # TODO: this amount should be zero, fees should be provided by aggregator pool
-            .with_amount(300_000)
             .send()
         )
 
@@ -47,19 +45,25 @@ class SandboxLineAggregatorTestCase(SandboxedJusterTestCase):
 
 
     def test_line_aggregator(self):
-        self._create_simple_event(self.manager)
-
         self._add_line(self.manager)
         self.bake_block()
 
-        # A deposits 10 tez:
-        self._deposit_liquidity(self.a, 10_000_000)
+        # A deposits 10 tez + fees to create two events in line:
+        event_creation_fees = self.line_aggregator.storage['newEventFee']() * 2
+        self._deposit_liquidity(self.a, 10_000_000 + event_creation_fees)
         self.bake_block()
 
         # A runs event:
-        self._aggregator_create_event(self.a)
+        opg = self._aggregator_create_event(self.a)
         self.bake_block()
+        result = self._find_call_result_by_hash(self.a, opg.hash())
 
-        # UNDER DEVELOPMENT: create event fails because there are no view in Juster
-        import pdb; pdb.set_trace()
+        # as far as two event in line are supposed, amount of provided liquidity
+        # should be 5tez:
+        event_params = self.juster.storage['events'][0]()
+        self.assertEqual(event_params['poolBelow'], 5_000_000)
+        self.assertEqual(event_params['poolAboveEq'], 5_000_000)
+
+        # TODO: run event with all measurements and some bets
+        # TODO: let provider withdraw
 
