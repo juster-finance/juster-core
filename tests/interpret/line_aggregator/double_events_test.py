0c39a4cccbfc8f1248312d3230b2bdc2b8c7b58b
ztepler <ztepler@gmail.com>
2021-12-06 17:36:15 +0300

    improved test to check edgecases

--- tests/interpret/line_aggregator/double_events_test.py
+++ tests/interpret/line_aggregator/double_events_test.py
@@ -5,6 +5,8 @@ from pytezos import MichelsonRuntimeError
 class DoubleEventsTest(LineAggregatorBaseTestCase):
     def test_double_event_should_fail(self):
 
+        PERIOD = 5*60
+
         # adding some liquidity so it will be possible to create events:
         self.deposit_liquidity(self.a, amount=3_000_000)
 
@@ -12,11 +14,18 @@ class DoubleEventsTest(LineAggregatorBaseTestCase):
         self.add_line(
             currency_pair='XTZ-USD',
             max_active_events=10,
-            bets_period=60*5
+            bets_period=PERIOD
         )
 
         # creating first event should succeed:
         self.create_event(event_line_id=0, next_event_id=0)
+
+        # waiting almost until lastBetsCloseTime:
+        lastBetsCloseTime = self.storage['lines'][0]['lastBetsCloseTime']
+        delta_before_bets_close = lastBetsCloseTime - self.current_time
+        self.assertTrue(delta_before_bets_close > 0)
+        self.assertTrue(delta_before_bets_close <= PERIOD)
+        self.wait(delta_before_bets_close - 1)
 
         # creating second event at the same time should fail:
         with self.assertRaises(MichelsonRuntimeError) as cm:
@@ -24,8 +33,8 @@ class DoubleEventsTest(LineAggregatorBaseTestCase):
         msg = 'Event cannot be created until previous event betsCloseTime'
         self.assertTrue(msg in str(cm.exception))
 
-        # trying with 5 min waiting:
-        self.wait(5*60)
+        # waiting to complete 5 minutes:
+        self.wait(1)
         self.create_event(event_line_id=0, next_event_id=1)
 
         self.assertEqual(len(self.storage['events']), 2)
