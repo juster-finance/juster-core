0966938b96aad31a8efa59bb0d49b1150d9561be
ztepler <ztepler@gmail.com>
2021-12-06 18:09:43 +0300

    added test with late event

---
+++ tests/interpret/line_aggregator/late_event_test.py
@@ -0,0 +1,32 @@
+from tests.interpret.line_aggregator.line_aggregator_base import LineAggregatorBaseTestCase
+from pytezos import MichelsonRuntimeError
+
+
+class LateEventTestCase(LineAggregatorBaseTestCase):
+    def test_that_event_should_be_created_in_the_future_when_late(self):
+
+        PERIOD = 5*60
+
+        # adding some liquidity so it will be possible to create events:
+        self.deposit_liquidity(self.a, amount=3_000_000)
+
+        # creating line with a lot of possible events and bets period 5 min:
+        self.add_line(
+            currency_pair='XTZ-USD',
+            max_active_events=3,
+            bets_period=PERIOD
+        )
+
+        # creating first event:
+        self.create_event(event_line_id=0, next_event_id=0)
+
+        # waiting a lot more time than period:
+        self.wait(PERIOD*10)
+
+        # creating second event and checking that it is created in good time:
+        self.create_event(event_line_id=0, next_event_id=1)
+        lastBetsCloseTime = self.storage['lines'][0]['lastBetsCloseTime']
+        delta_before_bets_close = lastBetsCloseTime - self.current_time
+        self.assertTrue(delta_before_bets_close > 0)
+        self.assertTrue(delta_before_bets_close <= PERIOD)
+
