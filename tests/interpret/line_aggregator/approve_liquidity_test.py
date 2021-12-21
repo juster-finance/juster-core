ab91db4fd50d9db579e4a229374fe37167d05a9a
ztepler <ztepler@gmail.com>
2021-12-21 23:07:00 +0300

    test that liquidity cannot be approved before time and can be approved after

--- tests/interpret/line_aggregator/approve_liquidity_test.py
+++ tests/interpret/line_aggregator/approve_liquidity_test.py
@@ -6,15 +6,14 @@ class ApproveLiquidityTestCase(LineAggregatorBaseTestCase):
     def test_should_fail_when_try_to_approve_liquidity_twice(self):
 
         # creating default event:
-        self.add_line(max_active_events=10)
+        self.add_line()
 
         # providing liquidity:
-        self.deposit_liquidity(self.a, amount=80_000_000)
-        self.approve_liquidity(self.a, entry_position_id=0)
-        self.assertEqual(self.storage['nextEventLiquidity'], 8_000_000)
+        self.deposit_liquidity()
+        self.approve_liquidity(entry_position_id=0)
 
         with self.assertRaises(MichelsonRuntimeError) as cm:
-            self.approve_liquidity(self.a, entry_position_id=0)
+            self.approve_liquidity(entry_position_id=0)
         msg = 'Entry position is not found'
         self.assertTrue(msg in str(cm.exception))
 
@@ -22,13 +21,42 @@ class ApproveLiquidityTestCase(LineAggregatorBaseTestCase):
     def test_should_fail_if_trying_to_approve_unexisted_entry_position(self):
 
         # creating default event:
-        self.add_line(max_active_events=10)
+        self.add_line()
 
         # providing liquidity:
-        self.deposit_liquidity(self.a, amount=80_000_000)
+        self.deposit_liquidity()
 
         with self.assertRaises(MichelsonRuntimeError) as cm:
-            self.approve_liquidity(self.a, entry_position_id=12)
+            self.approve_liquidity(entry_position_id=12)
         msg = 'Entry position is not found'
         self.assertTrue(msg in str(cm.exception))
+
+
+    def test_should_not_be_possible_to_approve_entry_position_before_time(self):
+
+        # different contract storage that includes lag for liquidity added:
+        self.storage['entryLockPeriod'] = 3600
+
+        # creating default event:
+        self.add_line()
+
+        # providing liquidity:
+        self.deposit_liquidity()
+
+        with self.assertRaises(MichelsonRuntimeError) as cm:
+            self.approve_liquidity(entry_position_id=0)
+        msg = 'Cannot approve liquidity before acceptAfter'
+        self.assertTrue(msg in str(cm.exception))
+
+        # waiting 1 second less that needed, still should not be able to approve:
+        self.wait(3599)
+
+        with self.assertRaises(MichelsonRuntimeError) as cm:
+            self.approve_liquidity(entry_position_id=0)
+        msg = 'Cannot approve liquidity before acceptAfter'
+        self.assertTrue(msg in str(cm.exception))
+
+        # waiting 1 second more and succeed to approve:
+        self.wait(1)
+        self.approve_liquidity(entry_position_id=0)
 
