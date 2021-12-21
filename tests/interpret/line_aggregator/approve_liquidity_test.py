b6a2052dc6c2c8cb8c3b9cbc3d959c80c7446512
ztepler <ztepler@gmail.com>
2021-12-21 22:58:25 +0300

    test that it is impossible to approve non existed entry position

--- tests/interpret/line_aggregator/approve_liquidity_test.py
+++ tests/interpret/line_aggregator/approve_liquidity_test.py
@@ -2,7 +2,7 @@ from pytezos import MichelsonRuntimeError
 from tests.interpret.line_aggregator.line_aggregator_base import LineAggregatorBaseTestCase
 
 
-class OneLiquidityApprovedTwiceTestCase(LineAggregatorBaseTestCase):
+class ApproveLiquidityTestCase(LineAggregatorBaseTestCase):
     def test_should_fail_when_try_to_approve_liquidity_twice(self):
 
         # creating default event:
@@ -15,6 +15,20 @@ class OneLiquidityApprovedTwiceTestCase(LineAggregatorBaseTestCase):
 
         with self.assertRaises(MichelsonRuntimeError) as cm:
             self.approve_liquidity(self.a, entry_position_id=0)
+        msg = 'Entry position is not found'
+        self.assertTrue(msg in str(cm.exception))
+
+
+    def test_should_fail_if_trying_to_approve_unexisted_entry_position(self):
+
+        # creating default event:
+        self.add_line(max_active_events=10)
+
+        # providing liquidity:
+        self.deposit_liquidity(self.a, amount=80_000_000)
+
+        with self.assertRaises(MichelsonRuntimeError) as cm:
+            self.approve_liquidity(self.a, entry_position_id=12)
         msg = 'Entry position is not found'
         self.assertTrue(msg in str(cm.exception))
 
