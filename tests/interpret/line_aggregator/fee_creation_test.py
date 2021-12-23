413015eac96db509e06b610ba372ecc5eca8ab32
ztepler <ztepler@gmail.com>
2021-12-23 20:58:03 +0300

    test fees calculation with nextEventLiquidity amounts

---
+++ tests/interpret/line_aggregator/fee_creation_test.py
@@ -0,0 +1,48 @@
+from tests.interpret.line_aggregator.line_aggregator_base import LineAggregatorBaseTestCase
+
+
+class FeeEventCreationTestCase(LineAggregatorBaseTestCase):
+    def test_next_event_liquidity_should_include_future_fees(self):
+
+        self.storage['newEventFee'] = 500_000
+
+        # creating default event:
+        self.add_line(max_active_events=3)
+
+        # providing liquidity:
+        self.deposit_liquidity(self.a, amount=4_500_000)
+        self.approve_liquidity(self.a, entry_position_id=0)
+        self.assertEqual(self.storage['nextEventLiquidity'], 1_500_000)
+
+        # second provider adds the same amount of liquidity:
+        self.deposit_liquidity(self.b, amount=4_500_000)
+        self.approve_liquidity(self.a, entry_position_id=1)
+
+        # and all of this liquidity should go to the events:
+        self.assertEqual(self.storage['nextEventLiquidity'], 3_000_000)
+
+        # creating one event:
+        self.create_event(event_line_id=0, next_event_id=0)
+        self.wait(3600)
+
+        # A decided to remove all liquidity so nextEventLiquidity should be /2:
+        withdrawn_amount = self.claim_liquidity(
+            self.a, position_id=0, shares=4_500_000)
+        self.assertEqual(self.storage['nextEventLiquidity'], 1_500_000)
+
+        # Run and finish event with profit 3xtz (provided 1xtz, 0.5xtz fee):
+        self.create_event(event_line_id=0, next_event_id=1)
+        self.wait(3600)
+        self.pay_reward(event_id=1, amount=4_000_000)
+        self.assertEqual(self.storage['nextEventLiquidity'], 2_500_000)
+
+        # Finishing first event with no profit (first event was created
+        # in 17th line with 2.5xtz + 0.5xtz fee):
+        self.pay_reward(event_id=0, amount=2_500_000)
+        self.assertEqual(self.storage['nextEventLiquidity'], 2_500_000)
+
+        # Creating another line that should increase next event creation reserves:
+        # 2.5 * 3 / (3+2) = 1.5
+        self.add_line(max_active_events=2)
+        self.assertEqual(self.storage['nextEventLiquidity'], 1_500_000)
+
