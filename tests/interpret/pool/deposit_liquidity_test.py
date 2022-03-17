952a9bcca723f0a4b78df75410c0b2d004cefe04
ztepler <ztepler@gmail.com>
2022-03-17 15:29:27 +0300

    added test case with full shares withdrawal and then deposit

--- tests/interpret/pool/deposit_liquidity_test.py
+++ tests/interpret/pool/deposit_liquidity_test.py
@@ -16,3 +16,16 @@ class DepositLiquidityTestCase(PoolBaseTestCase):
         self.add_line()
         self.deposit_liquidity(amount=1000)
 
+
+    def test_should_allow_to_withdraw_all_liquidity_and_add_new(self):
+        self.add_line()
+        self.deposit_liquidity(amount=1000)
+        self.approve_liquidity(entry_id=0)
+        self.assertEqual(self.storage['totalShares'], 1000)
+        self.claim_liquidity(position_id=0, shares=1000)
+        self.assertEqual(self.storage['totalShares'], 0)
+
+        self.deposit_liquidity(amount=1000)
+        self.approve_liquidity(entry_id=1)
+        self.assertEqual(self.storage['totalShares'], 1000)
+
