c5c48b0e802a9fd56a151cdb04fee76864cab31c
ztepler <ztepler@gmail.com>
2022-03-21 11:37:49 +0300

    big number test added

---
+++ tests/interpret/pool/big_numbers_test.py
@@ -0,0 +1,25 @@
+from tests.interpret.pool.pool_base import PoolBaseTestCase
+from pytezos import MichelsonRuntimeError
+
+
+class BigNumbersTestCase(PoolBaseTestCase):
+
+    def test_should_allow_to_provide_trillions(self):
+        big_amount = 10**18
+        self.add_line(max_events=2)
+        self.deposit_liquidity(amount=big_amount)
+        position_id = self.approve_liquidity()
+        first_event = self.create_event()
+        self.wait(3600)
+        self.claim_liquidity(shares=int(big_amount*0.2))
+        second_event = self.create_event()
+        self.pay_reward(event_id=first_event, amount=int(big_amount*0.5))
+        self.wait(3600)
+        self.claim_liquidity(shares=int(big_amount*0.8))
+        self.pay_reward(event_id=second_event, amount=int(big_amount*0.5*0.8))
+        self.withdraw_liquidity(positions=[
+            {'positionId': position_id, 'eventId': first_event},
+            {'positionId': position_id, 'eventId': second_event}
+        ])
+
+
