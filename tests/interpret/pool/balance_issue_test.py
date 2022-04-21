d3b70910ad967fe43f442ccdf6df24390fc93013
ztepler <ztepler@gmail.com>
2022-04-21 14:08:06 +0300

    reproduced ithacanet pool error found during testing

---
+++ tests/interpret/pool/balance_issue_test.py
@@ -0,0 +1,33 @@
+from tests.interpret.pool.pool_base import PoolBaseTestCase
+from pytezos import MichelsonRuntimeError
+
+
+class BalanceIssueTestCase(PoolBaseTestCase):
+
+    def test_should_allow_participants_claim_if_their_claims_rounded_againts_pool(self):
+        self.add_line(max_events=3)
+        entry_id = self.deposit_liquidity(amount=1_000_000)
+        position_one = self.approve_liquidity(entry_id=entry_id)
+
+        entry_id = self.deposit_liquidity(amount=1_000_001)
+        position_two = self.approve_liquidity(entry_id=entry_id)
+
+        first_event = self.create_event()
+        self.wait(3600)
+        second_event = self.create_event()
+        self.wait(3600)
+
+        self.claim_liquidity(shares=1_000_000, position_id=position_one)
+        self.claim_liquidity(shares=1_000_001, position_id=position_two)
+        assert self.balances['contract'] >= 0
+
+        self.pay_reward(event_id=first_event, amount=1_000_000)
+        self.pay_reward(event_id=second_event, amount=1_000_000)
+
+        self.withdraw_liquidity(positions=[
+            {'positionId': position_one, 'eventId': first_event},
+            {'positionId': position_one, 'eventId': second_event},
+            {'positionId': position_two, 'eventId': first_event},
+            {'positionId': position_two, 'eventId': second_event}
+        ])
+
