45af2f6dba289a34ffbfea2783d001c74ff99d87
ztepler <ztepler@gmail.com>
2022-05-17 23:49:46 +0300

    added pool model test for equality

---
+++ tests/interpret/pool/pool_model_test.py
@@ -0,0 +1,14 @@
+from tests.interpret.pool.pool_model import PoolModel
+from copy import deepcopy
+from unittest import TestCase
+from decimal import Decimal
+
+
+class PoolModelTestCase(TestCase):
+    def test_should_fail_to_cmp_different_models_after_copy(self):
+        model = PoolModel()
+        model_copy = deepcopy(model)
+        self.assertEqual(model_copy, model)
+        model_copy.default(Decimal(1_000_000))
+        self.assertNotEqual(model_copy, model)
+
