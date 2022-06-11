83e5b473f6e1cfedf8bf66d567b2fc2c9b974fc2
ztepler <ztepler@gmail.com>
2022-06-11 23:09:13 +0300

    added create event and withdraw to model random test

--- tests/model/pool/random_test.py
+++ tests/model/pool/random_test.py
@@ -5,14 +5,21 @@ from models.pool import PoolModel
 from random import choice, randint, seed
 
 
+def rand_from_zero_to(value_to):
+    return randint(0, value_to) if value_to > 0 else value_to
+
+
 class RandomTester:
     users = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'manager']
+    max_events = 18
 
     def __init__(self, model):
         self.model = model
         self.seed = randint(0, 10**16)
         self.balances = {user: Decimal(0) for user in self.users}
+        self.balances['juster_core'] = Decimal(0)
         seed(self.seed)
+        self.next_event_id = 0
 
     def check_invariants(self):
         assert sum(self.balances.values()) + self.model.balance == 0
@@ -49,14 +56,34 @@ class RandomTester:
             return
         position_id = choice(list(self.model.positions.keys()))
         position = self.model.positions[position_id]
-        random_shares = (
-            randint(1, position.shares) if position.shares > 1 else 0
-        )
+        random_shares = rand_from_zero_to(position.shares)
+
         payout = self.model.claim_liquidity(
             position_id=position_id,
             shares=random_shares
         )
         self.balances[position.provider] += payout
+
+    def random_create_event(self):
+        if len(self.model.active_events) > self.max_events:
+            return
+        if self.model.calc_free_liquidity_f() == 0:
+            return
+
+        self.balances['juster_core'] += self.model.calc_next_event_liquidity()
+        self.model.create_event(line_id=0, next_event_id=self.next_event_id)
+        self.next_event_id += 1
+
+    def random_withdraw(self):
+        claim_keys = list(self.model.claims.keys())
+        claims_count = rand_from_zero_to(len(claim_keys))
+
+        while len(claim_keys) > claims_count:
+            claim_keys.pop(rand_from_zero_to(len(claim_keys) - 1))
+
+        payouts = self.model.withdraw_liquidity(claim_keys)
+        for user, payout in payouts.items():
+            self.balances[user] += payout
 
     def random_action(self):
         actions = [
@@ -64,11 +91,25 @@ class RandomTester:
             *[self.random_cancel] * 1,
             *[self.random_approve] * 6,
             *[self.random_claim] * 5,
+            *[self.random_withdraw] * 5,
+            *[self.random_create_event] * 20,
+            # *[self.random_pay_reward] * 20,
+            # *[self.random_default] * 10,
+            # TODO: random trigger pause line/deposits?
         ]
         action = choice(actions)
         action()
 
     def run(self, iterations=1000):
+        self.model.add_line(
+            measure_period=3600,
+            bets_period=3600,
+            last_bets_close_time=0,
+            max_events=self.max_events,
+            is_paused=False,
+            min_betting_period=0,
+        )
+
         for iteration in range(iterations):
             self.random_action()
             self.check_invariants()
